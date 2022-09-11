4582a6449cdfa050c06f9bcc8151c5890833fba3
Egor Zhelobanov <e.zhelobanov@gmail.com>
2022-09-11 17:13:22 +1100

    Fixed homework 10. Added task 2

--- homeworks/NetologyHomeWork10.py
+++ homeworks/NetologyHomeWork10.py
@@ -1,6 +1,8 @@
 import requests
 import json
+import os.path
 
+# Задача 1
 response = requests.get('https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/all.json')
 
 if response.status_code != 200:
@@ -11,3 +13,21 @@ hero_dict = json.loads(response.text)
 for hero in hero_dict:
     if hero['name'] == 'Hulk' or hero['name'] == 'Captain America' or hero['name'] == 'Thanos':
         print(hero['name'] + ', интеллект - ' + str(hero['powerstats']['intelligence']))
+
+
+# Задача 2
+GIFS_DIR = "gifs"
+HEADERS = {"Authorization": "OAuth <TOKEN_HERE>"}
+
+resp1 = requests.get("https://cloud-api.yandex.net/v1/disk/resources/upload",
+                     params={"path": "/awesome.gif", "owerwrite": "true"},
+                     headers=HEADERS)
+d1 = resp1.json()
+href = d1["href"]
+
+with open(os.path.join(GIFS_DIR, "<FILENAME_HERE>"), "rb") as f:
+    resp2 = requests.put(href, files={"file": f})
+
+resp2.raise_for_status()
+print("File has been successful uploaded!")
+# print(resp2.json())
